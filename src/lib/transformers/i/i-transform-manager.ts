503dae26dbcbcbce99485162fb9e6c6107a0493b
chris_dibbern <chrisdibbern@gmail.com>
2017-07-01 18:01:51 -0500

    Oops, adjusting method def in transform manager.

--- src/lib/transformers/i/i-transform-manager.ts
+++ src/lib/transformers/i/i-transform-manager.ts
@@ -2,5 +2,5 @@ import * as i from '../../i/template';
 
 export interface ITransformManager {
     configure(tmpl: i.ITemplate, inputs: { [key: string]: any }): void;
-    applyTransforms(path: string, content: string, replaceDef: i.IReplacementDefinition, inputs: { [key: string]: any }): string;
+    applyTransforms(path: string, content: string, replaceDef: i.IReplacementDefinition): string;
 }
