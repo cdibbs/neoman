d6378310945cd9aa44071c6c9b1400a5e0956ff4
chris_dibbern <chrisdibbern@gmail.com>
2017-07-05 16:41:52 -0500

    Refactored neoman.js browser prompt lib

--- readme.md
+++ readme.md
@@ -1,11 +1,52 @@
 # Neoman Template Manager
+neoman - _Old Saxon_. None, nobody.
+
+A template manager that intends to be _nobody_ and _nothing_; it does not want to impose on your code. Neoman knows that your code shouldn't have to become a template, for it already is one.
+
+Drop a single, unobtrusive folder into your project, and Neoman will have all it needs.
+
+`MyProject/.template.config/template.json`
+
+A minimal `template.json` would look like this:
+
+```json
+{
+    "name": "My Project",
+    "description": "My useful project will lead many to triumph.",
+    "author": "Noh Body",
+    "url": "https://en.wikipedia.org/wiki/Outis",
+    "identity": "myproj",
+
+    "inputConfig": {
+        "defaultInterface": "prompt",
+        "define": {
+            "namespace": "What will the root namespace of your project be?"
+        }
+    },
+
+    "replace": [
+        { "replace": "my.project.namespace", "with": "{{namespace}}" }
+    ],
+
+    "#": "Really, you don't need 'files' if you want the 'replace' section to apply to all files",
+    "files": ["**/*.ts"]
+}
+```
+
+## Documentation
+
 For documentation, please refer to [the wiki](https://github.com/cdibbs/neoman/wiki).
 
 ## Building
 
-To build neoman from source, clone the repository and run:
+To build and run Neoman from source, clone the repository and, from its directory, run:
 
-`npm run build`
+```
+npm run build
+npm link
+neoman
+```
+
 
 ## Authors
 
--- src/browser-prompt/scripts/neoman.js
+++ src/browser-prompt/scripts/neoman.js
@@ -6,34 +6,85 @@ $(document).ready(function() {
      * The intention is to provide a more seamless user experience.
      * 
      * If building a custom input page, include this script to get the above described behavior.
-     */
-
-    // If no connection after X seconds browser-side, warn the user.
-    // If no connection server-side, inform the user in the console.
-    // If connection broken, inform in console.
+     * 
+     * Client scripts may listen for events to handle user interaction or event cancellation.
+     * For cancellation, your script must load and register the appropriate events before neoman.js.
+     * 
+     * Examples:
+     *   - $(window).bind('neoman.[event]', myHandler);
+     *   - window.addEventListener('neoman.[event]', myHandler, false);
+     * 
+     * Events: */
+    const neoman_disconnected = "neoman.disconnected"; // When we no longer hear from the Neoman internal HTTP server.
+    const neoman_connected = "neoman.connected"; // Based on ws.onopen.
+    const neoman_unload = "neoman.unload"; // When we're about to send an unload notification to the Neoman server. Based on browser beforeunload.
+    const neoman_load = "neoman.load"; //  When we're about to send a load notification. Based on WebSocket.onopen.
+    const neoman_ws_error = "neoman.ws.error"; // WebSocket errors.
+    const neoman_ws_message = "neoman.ws.message"; // Messages from Neoman. Not currently used.
 
     var ws = new WebSocket("ws://localhost:3638");
-    ws.onopen = function(event) {
-        ws.send(JSON.stringify({
-            eventType: "load"
-        }));
-    };
-    ws.onerror = function(event) {
-        // Should we do something with this?
-        console && console.log(event);
-    };
+    setupUserEvents(ws);
+    setupLowLevelEvents(ws); // trigger user-land events.
 
-    ws.onmessage = function(msgEvent) {
-        // Not expecting anything beyond pings from Neoman.
-        console && console.log(msgEveent);
-    };
+    function setupUserEvents(ws) {
+        $(window).bind(neoman_connected, function() {
+            ws.send(JSON.stringify({
+                eventType: "load"
+            }));
+        });
 
-    $(window).bind("beforeunload", function() { 
-        ws.send(JSON.stringify({
-            eventType: "unload"
-        }));
+        $(window).bind(neoman_unload, function() {
+            // Let the primary user page/library handle warning the user about unload.
+            // return confirm("Do you really want to leave?"); 
+            ws.send(JSON.stringify({
+                eventType: "unload"
+            }));
+        });
 
-        // Let the primary user input page/library handle this:
-        // return confirm("Do you really want to close?"); 
-    });
+        $(window).bind(neoman_ws_error, function(err) {
+            console && console.log(event);
+        });
+
+        $(window).bind(neoman_ws_message, function(msgEvent) {
+            console && console.log(msgEveent);
+        });
+
+        $(window).bind(neoman_disconnected, function() {
+            console && console.warn('Neoman connection lost.');
+            // Do nothing? No way to inform server. It probably knows, anyway.
+        });
+    }    
+
+    function setupLowLevelEvents(ws) {
+        ws.onopen = function(event) {
+            $(window).trigger(neoman_connected);
+        };
+        ws.onerror = function(event) {
+            // Should we do something with this?
+            $(window).trigger(neoman_ws_error, event);
+        };
+
+        ws.onmessage = function(msgEvent) {
+            // Not expecting anything beyond pings from Neoman.
+            $(window).trigger(neoman_ws_message, msgEvent);
+        };
+
+        $(window).bind("beforeunload", function() { 
+            $(window).trigger(neoman_unload);
+        });
+
+        const interval = setInterval(function ping() {
+            if (ws.isAlive === false)
+            {   
+                $(window).trigger(neoman_disconnected);
+                return ws.terminate();
+            }
+
+            ws.isAlive = false;
+            ws.ping('', false, true);
+        }, 1000);
+
+        // If we're here, we're all loaded and setup.
+        $(window).trigger(neoman_load);
+    }
 });
