46b393ebbbac64647c95f5e9ac599c0295622438
chris_dibbern <chrisdibbern@gmail.com>
2017-07-16 20:46:24 -0500

    Added list command test

---
+++ src/lib/commands/list-command.spec.ts
@@ -0,0 +1,80 @@
+/// <reference path="../../../node_modules/@types/mocha/index.d.ts" />
+/// <reference path="../../../node_modules/@types/chai/index.d.ts" />
+import "reflect-metadata";
+import 'mocha';
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+let expect = chai.expect, assert = chai.assert;
+import * as i from '../i';
+import * as nci from './i';
+import { ITemplate } from '../i/template';
+
+import { ListCommand } from './list-command';
+
+describe('ListCommand', () => {
+    let c: ListCommand;
+    beforeEach(() => {
+        let userMessager: i.IUserMessager = {
+            info: (message: any, indent?: number): void => {},
+            debug: (message: any, indent?: number): void => {},
+            warn: (message: any, indent?: number): void => {},
+            error: (message: any, indent?: number): void => {},
+            write: (message: string, indent: number = 0, level: i.Levels = i.LEVELS.Debug): void => {}
+        };
+        c = new ListCommand(userMessager, <NodeJS.Process>{}, <i.IFileSystem>{ }, <i.IPath>{}, <i.IGlob>{});
+        c.tempDir = "/tmp/mytemplates";
+    });
+
+    describe('#run', () => {
+        it('should set the Glob to use tempDir, and bind match and end', () => {
+            let onSpy = sinon.spy(), bindStub = sinon.stub();
+            let globStub = sinon.stub();
+            globStub.returns({ on: onSpy});
+            bindStub.withArgs(c.match).returns(c.match);
+            bindStub.withArgs(c.end).returns(c.end);
+
+            c["glob"].Glob = <any>globStub;
+            c["bind"] = bindStub;
+            let opts = {}, args = {};
+            let result = c.run(opts, args);
+            sinon.assert.calledWith(globStub, c.neomanPath, { cwd: c.tempDir });
+            sinon.assert.calledWith(onSpy, "match", c.match);
+            sinon.assert.calledWith(onSpy, "end", c.end);
+        });
+    });
+
+    describe("#match", () => {
+        it('should catch errors and exit process.', () => {
+            let exitSpy = sinon.spy();
+            let readFileSyncStub = sinon.stub(), joinStub = sinon.stub();
+            readFileSyncStub.throws("bad");
+            joinStub.returns("/tmp/templates/myfile.txt");
+            c["path"].join = joinStub;
+            c["process"].exit = <any>exitSpy;
+            c["fs"].readFileSync = <any>readFileSyncStub;
+            c.match("myfile.txt");
+            expect(exitSpy.called).to.be.true;
+            sinon.assert.calledWith(exitSpy, 1);
+        });
+
+        it('should not error on reading valid JSON.', () => {
+            let exitSpy = sinon.spy();
+            let readFileSyncStub = sinon.stub(), joinStub = sinon.stub();
+            readFileSyncStub.returns("{}");
+            joinStub.returns("/tmp/templates/myfile.txt");
+            c["path"].join = joinStub;
+            c["process"].exit = <any>exitSpy;
+            c["fs"].readFileSync = <any>readFileSyncStub;
+            c.match("myfile.txt");
+            expect(exitSpy.called).to.be.false;
+        });
+    });
+
+    describe("#end", () => {
+        it('should not error.', () => {
+            assert.doesNotThrow(() => c.end(["one", "two"]));
+            assert.doesNotThrow(() => c.end([]));
+            assert.doesNotThrow(() => c.end(null));
+        });
+    });
+});
--- src/lib/commands/list-command.ts
+++ src/lib/commands/list-command.ts
@@ -8,6 +8,7 @@ import { IFileSystem, IGlob, IPath, IUserMessager } from '../i';
 @injectable()
 export class ListCommand extends BaseCommand<any, any> {
     type: Commands = COMMANDS.ListTemplates;
+    neomanPath: string = "*/.neoman.config/template.json";
 
     constructor(
         @inject(TYPES.UserMessager) protected msg: IUserMessager,
@@ -25,23 +26,28 @@ export class ListCommand extends BaseCommand<any, any> {
         this.msg.info("Listing templates in your template directory.");
         this.msg.info(`Using: ${this.tempDir}\n`);
 
-        let g = new this.glob.Glob("*/.neoman.config/template.json", { cwd: this.tempDir });
-        g.on("match", this.match.bind(this));
-        g.on("end", this.end.bind(this));
+        let g = new this.glob.Glob(this.neomanPath, { cwd: this.tempDir });
+        g.on("match", this.bind(this.match));
+        g.on("end", this.bind(this.end));
     }
 
     match(file: string): any {
+        try {
             let fullPath = this.path.join(this.tempDir, file);
-            try {
-                let tmpl = JSON.parse(this.fs.readFileSync(fullPath, 'utf8'));
-                this.msg.info(`\t${tmpl.identity} - ${tmpl.name}`);
-            } catch (ex) {
-                this.msg.error(`Error reading template definition file: ${file}.`);
-                throw ex;
-            }
+            let tmpl = JSON.parse(this.fs.readFileSync(fullPath, 'utf8'));
+            this.msg.info(`\t${tmpl.identity} - ${tmpl.name}`);
+        } catch (ex) {
+            this.msg.error(`Error reading template definition file: ${file}.`);
+            this.msg.error(ex.stack);
+            this.process.exit(1);
+        }
     }
 
     end(allFiles: string[]): any {
-        this.msg.info(`\n${allFiles.length} template(s) found.\n`);
+        this.msg.info(`\n${(allFiles || []).length} template(s) found.\n`);
+    }
+
+    bind<T extends (...args: any[]) => any>(fn: T): T {
+        return fn.bind(this);
     }
 }
